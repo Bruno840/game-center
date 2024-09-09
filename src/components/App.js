3928019f20ee735121f4db94e83edbd82460453d
Bruno840 <rdac2PcA3d>
2024-09-09 17:13:47 +0100

    Created the project

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './App.css';
+import Header from './Header';
+import Trivia from './Trivia';
+
+
+
+function App() {
+
+  return (
+    <div className='app'>
+      <Header></Header>
+      <p>This is the Game Center.</p>
+     
+      <BrowserRouter>
+      <div className='menu container'>
+        <h2>Menu</h2>
+        <p>
+            <Link to="/">Home</Link>
+        </p>
+        <p>
+            <Link to="/trivia">Trivia</Link>
+        </p>
+      </div>
+        <Routes>
+          <Route path='/trivia' element={<Trivia/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './App.css'
+
+const Header = () => {
+    return(
+        <div className="header container">
+            <div className="header center">
+                <h2>Game Center</h2>
+            </div>
+        </div>
+    );
+};
+export default Header;
